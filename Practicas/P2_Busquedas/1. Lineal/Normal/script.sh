79525955a018bb2daf2efaebead421ffcf9ab270
erik <erik.mofe@gmail.com>
2022-11-18 09:44:51 -0600

    Final con hilos

--- Practicas/P2_Busquedas/1. Lineal/Normal/script.sh
+++ Practicas/P2_Busquedas/1. Lineal/Normal/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-gcc lineal.c tiempo.c
-./busqueda.exe 1000000 < 'n10millonesDes.txt' > salida.txt
-./busqueda.exe 2000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 3000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 4000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 5000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 6000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 7000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 8000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 9000000 < 'n10millonesDes.txt' >> salida.txt
-./busqueda.exe 10000000 < 'n10millonesDes.txt' >> salida.txt
+gcc lineal.c tiempo.c -o busqueda.exe
+./busqueda.exe 1000000 < numDes.txt > salida.txt
+./busqueda.exe 2000000 < numDes.txt >> salida.txt
+./busqueda.exe 3000000 < numDes.txt >> salida.txt
+./busqueda.exe 4000000 < numDes.txt >> salida.txt
+./busqueda.exe 5000000 < numDes.txt >> salida.txt
+./busqueda.exe 6000000 < numDes.txt >> salida.txt
+./busqueda.exe 7000000 < numDes.txt >> salida.txt
+./busqueda.exe 8000000 < numDes.txt >> salida.txt
+./busqueda.exe 9000000 < numDes.txt >> salida.txt
+./busqueda.exe 10000000 < numDes.txt >> salida.txt
--- Practicas/P2_Busquedas/2. En ABB/HIlos/script.sh
+++ Practicas/P2_Busquedas/2. En ABB/HIlos/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-dir="/mnt/c/Users/erikm/Desktop/algoritmos/Practicas/P2_Busquedas/10millones.txt"
-gcc linealConHIlos.c tiempo.c -o busqueda.exe
-./busqueda.exe 1000000 < $dir > salida.txt
-./busqueda.exe 2000000 < $dir >> salida.txt
-./busqueda.exe 3000000 < $dir >> salida.txt
-./busqueda.exe 4000000 < $dir >> salida.txt
-./busqueda.exe 5000000 < $dir >> salida.txt
-./busqueda.exe 6000000 < $dir >> salida.txt
-./busqueda.exe 7000000 < $dir >> salida.txt
-./busqueda.exe 8000000 < $dir >> salida.txt
-./busqueda.exe 9000000 < $dir >> salida.txt
-./busqueda.exe 10000000 < $dir >> salida.txt
+gcc abb.c arbolBinario.c tiempo.c -lpthread -o busqueda.exe
+./busqueda.exe 1000000 < numDes.txt > salida.txt
+./busqueda.exe 2000000 < numDes.txt >> salida.txt
+./busqueda.exe 3000000 < numDes.txt >> salida.txt
+./busqueda.exe 4000000 < numDes.txt >> salida.txt
+./busqueda.exe 5000000 < numDes.txt >> salida.txt
+./busqueda.exe 6000000 < numDes.txt >> salida.txt
+./busqueda.exe 7000000 < numDes.txt >> salida.txt
+./busqueda.exe 8000000 < numDes.txt >> salida.txt
+./busqueda.exe 9000000 < numDes.txt >> salida.txt
+./busqueda.exe 10000000 < numDes.txt >> salida.txt
